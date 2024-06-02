bfd4eeb7a889fc412bcddfe4605be264ebf34ce7
BmanStudio <oribennoon@gmail.com>
2024-06-02 21:22:50 +0300

    WIP - trying to add a CSV parser

---
+++ fstreamPractice/csvHandler.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <string>
+using namespace std;
+
+class csvHandler
+{
+public:
+    void parseData(string fileName = "data.csv");
+
+private:
+    string data[100][100];
+};
---
+++ fstreamPractice/fstreamPractice.cpp
@@ -0,0 +1,9 @@
+
+#include "csvHandler.h"
+
+int main(int argc, char* argv[])
+{
+    csvHandler csv_handler;
+    csv_handler.parseData();
+    return 0;
+}
